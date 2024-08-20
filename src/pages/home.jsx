f2d47f8fb37b56e9e1f4821b690242f0ce3c5442
afsheen483 <afsheenakhtar483@gmail.com>
2024-08-20 22:47:40 +0500

    update

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function HomeScreen() {
   // Handler function for button clicks
   const handleClick = (language) => {
-          alert(`You click the ${language} button!`);
+          alert(`You clicked the ${language} button!`);
   };
 
   return (
